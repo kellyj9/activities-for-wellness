d99838576b41f3522c3cc84763fe68eb0eab3446
Kelly Jones <kellymj6@gmail.com>
2021-11-23 23:54:04 -0500

    Initial commit

---
+++ src/main/java/com/kellymariejones/activitiesforwellness/ActivitiesForWellnessApplication.java
@@ -0,0 +1,13 @@
+package com.kellymariejones.activitiesforwellness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiesForWellnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiesForWellnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kellymariejones/activitiesforwellness/ActivitiesForWellnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kellymariejones.activitiesforwellness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiesForWellnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
