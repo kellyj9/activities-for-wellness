c133e9de6dc662cc6ddb6ed79fd263d57a35de7f
Kelly Jones <kellymj6@gmail.com>
2021-12-03 08:35:27 -0500

    Added annotation in SampleRepository

--- src/main/java/com/kellymariejones/activitiesforwellness/data/SampleRepository.java
+++ src/main/java/com/kellymariejones/activitiesforwellness/data/SampleRepository.java
@@ -2,9 +2,11 @@ package com.kellymariejones.activitiesforwellness.data;
 
 import com.kellymariejones.activitiesforwellness.models.Sample;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface SampleRepository extends CrudRepository<Sample, Integer> {
 
     List<Sample> findAllByDimensionId(Integer dimension_id);
